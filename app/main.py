c5325ffc01d98c28f956456338c1c6d7ae5cb205
vinayvmathad <vinaymatad@gmail.com>
2025-05-10 21:21:57 +0530

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import SessionLocal, engine, Base
+from . import crud
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/tasks/")
+def read_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks(db)
+
+@app.post("/tasks/")
+def add_task(title: str, db: Session = Depends(get_db)):
+    return crud.create_task(db, title)
+
+@app.put("/tasks/{task_id}")
+def mark_done(task_id: int, db: Session = Depends(get_db)):
+    return crud.complete_task(db, task_id)
---
+++ test/test_main.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_tasks():
+    response = client.get("/tasks/")
+    assert response.status_code == 200
