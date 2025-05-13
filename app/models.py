714822e38a43331e0bd6490284adf259b51c7e51
vinayvmathad <vinaymatad@gmail.com>
2025-05-13 06:52:08 +0000

    Edit models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
+
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
