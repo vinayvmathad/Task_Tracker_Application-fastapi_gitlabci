b6f63e0433ac64960875310371125bf5f1977be5
vinayvmathad <vinaymatad@gmail.com>
2025-05-13 06:20:47 +0000

    Edit test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,10 @@
+# test/test_main.py
 from fastapi.testclient import TestClient
 from app.main import app
+from app.database import Base, engine
+
+# Create all tables before running tests
+Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
